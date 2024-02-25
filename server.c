b89aebade429c83cc3a243544507358140625a04
Yumiowari <owariyumi@gmail.com>
2024-02-25 01:31:05 -0300

    #2
    estrutura básica o servidor feita.

---
+++ server.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h> // isdigit()
+#include <signal.h> // signal()
+#include <unistd.h> // fork(), pipe(), etc.
+#include <arpa/inet.h> // manipulação e conversão de endereços IP
+
+#define MAX 10 // nº máximo de clientes
+
+int stop = 0;
+
+void handle_sigint(int signum);
+// tratamento do sinal SIGINT (Ctrl+C)
+
+int main(int argc, char **argv){
+    int i = 0; // contador
+    int port; // porta
+    int server_socket; // soquete do servidor
+    struct sockaddr_in server_addr; // endereço do servidor
+
+    // trata os argumentos de execução
+    if(argc < 2){
+        fprintf(stderr, "Argumentos insuficientes. Uso: %s <porta>\n", argv[0]);
+
+        exit(EXIT_FAILURE);
+    }else if(argc > 2){
+        fprintf(stderr, "Muitos argumentos. Uso: %s <porta>\n", argv[0]);
+
+        exit(EXIT_FAILURE);
+    }else{
+        for(i = 0; i < strlen(argv[1]); i++){
+            if(!isdigit(argv[1][i])){
+                fprintf(stderr, "A porta deve ser um inteiro.\n");
+
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        port = atoi(argv[1]);
+    }
+    //
+
+    signal(SIGINT, handle_sigint);
+
+    // criando o soquete do servidor
+    printf("Criando o soquete do servidor...\n");
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_socket == -1){
+        perror("Erro ao criar o soquete do servidor.\n");
+
+        exit(EXIT_FAILURE);
+    }
+    //
+
+    // configurando o endereço do servidor
+    printf("Configurando o endereço do servidor...\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    //
+
+    // vinculando o soquete à porta
+    printf("Vinculando o soquete à porta %d...\n", port);
+    if(bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+        perror("Erro ao vincular o soquete à porta.\n");
+
+        exit(EXIT_FAILURE);
+    }
+    //
+
+    // inicia o servidor
+    if(listen(server_socket, MAX) == -1){
+        perror("Erro ao iniciar o servidor.\n");
+
+        exit(EXIT_FAILURE);
+    }
+    //
+
+    printf("\nO servidor on-line e escutando na porta %d!\n", port);
+
+    // loop do servidor
+    while(!stop);
+    //
+
+    close(server_socket);
+
+    exit(EXIT_SUCCESS);
+}
+
+void handle_sigint(int signum){
+    printf("\nEncerrando o servidor...\n");
+
+    stop = 1;
+}
